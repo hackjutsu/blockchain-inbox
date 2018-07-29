786cc574a63a3b388a9e76fae2c434adc7e5be32
hackjutsu <cosmo.lepton@gmail.com>
2018-07-29 13:24:43 -0700

    add eslint

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
- // Web3 is a constructor function, which we usually capitalize.
+// Web3 is a constructor function, which we usually capitalize.
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
@@ -16,7 +16,7 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface)) // teach web3 about what methods an Inbox contract has
     .deploy({ data: bytecode, arguments: ['Hi there!'] }) // tell web3 that we want to deploy a new copy of this contract
     .send({ from: accounts[0], gas: '1000000' }); // instructs web3 to send out a transaction that creates this contract
-  });
+});
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
